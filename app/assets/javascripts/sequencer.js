c06dda29ec61d4c233ed763cf4b440a4532faa6d
artfoundry <dave@artfoundry.com>
2013-11-04 22:16:48 -0800

    Adding multiple instruments and channels
    * 14 instrument files added to soundfont
    * Now able to control instrument with buttons
    * Now able to control output channel with buttons (for future use with multiple users, ie. multiple instruments playing simultaneously)
    * Modified note highlighting so actual played note is highlighted

--- app/assets/javascripts/sequencer.js
+++ app/assets/javascripts/sequencer.js
@@ -1,7 +1,7 @@
 // Note IDs:
-// Each time slot has 16 notes (2 octaves), numbered 0-15 + (16 x time) 
+// Each time slot has 15 notes (2 octaves), numbered 0-14 + (15 x time) 
 // where time is numbered 0-15 (4 bars)
-// Thus, notes are numbered 0-255, which corresponds to the indecies of the notes array
+// Thus, notes are numbered 0-239, which corresponds to the indecies of the notes array
 
 $(document).ready(function(){
   initSequencer();
@@ -10,10 +10,24 @@ $(document).ready(function(){
 function initSequencer() {
   MIDI.loadPlugin({
     soundfontUrl: "./assets/soundfont/",
-    instrument: "acoustic_grand_piano",
+    instruments: ["acoustic_grand_piano", 
+                  "acoustic_guitar_nylon", 
+                  "acoustic_guitar_steel", 
+                  "alto_sax", 
+                  "baritone_sax", 
+                  "brass_section", 
+                  "distortion_guitar", 
+                  "electric_bass_finger", 
+                  "electric_bass_pick", 
+                  "electric_guitar_jazz", 
+                  "flute", 
+                  "soprano_sax", 
+                  "synth_drum", 
+                  "tenor_sax", 
+                  "trumpet"],
     callback: function() {
       newSeq = new Sequence();
-      newSeq.initGrid();
+      initGrid(newSeq);
       newSeq.initNotes();
       newSeq.addListener();
     }
@@ -46,30 +60,33 @@ function convertNoteIdToValue(noteId) {
   return value;
 };
 
-function calcDelay(tempo){
+function calcDelay(tempo) {
   var delay = 1000 / (tempo / 60);
   return delay;
-}
+};
+
+function initGrid(sequence) {
+  for (var row = totalOctaveNotes - 1; row >= 0; row--) {
+    $("#sequence").append("<div id='row" + row + "'>");
+    for (var col = 0; col < sequenceLength; col++) {
+      $("#sequence").append("<button class='note' id='" + (row + (col * totalOctaveNotes)) + "'>" + (row + (col * totalOctaveNotes)) + "</button>");
+    };
+    $("#sequence").append("</div>");
+  };
+};
+
 
 function Sequence () {
   maxOctave = 2;
   totalOctaveNotes = maxOctave * 8 - 1;
   sequenceLength = 16;
   allNotesInSeq = totalOctaveNotes * sequenceLength;
+  currentInstrument = 0; // ie. acoustic grand piano
+  channel = 0; //channel for each instrument
   tempo = 120;
   playOn = false;
   notes = [];
   time = 0;
-};
-
-Sequence.prototype.initGrid = function () {
-  for (var row = totalOctaveNotes - 1; row >= 0; row--) {
-    $("#sequence").append("<div id='row" + row + "'>");
-    for (var col = 0; col < sequenceLength; col++) {
-      $("#sequence").append("<button class='button' id='" + (row + (col * totalOctaveNotes)) + "'>" + (row + (col * totalOctaveNotes)) + "</button>");
-    };
-    $("#sequence").append("</div>");
-  };
 };
 
 Sequence.prototype.initNotes = function () {
@@ -82,6 +99,7 @@ Sequence.prototype.addListener = function () {
   $("#sequence").click(function(event) {
     if (event.target.id === "play"){
       if (playOn === false) {
+        $("#play").addClass('selected');
         playOn = true;
         playSeq = setInterval(function() {
           newSeq.playNotes(time);
@@ -89,13 +107,24 @@ Sequence.prototype.addListener = function () {
         }, calcDelay(tempo));
       }
       else {
+        $("#play").removeClass('selected');
         playOn = false;
         clearInterval(playSeq);
       };
     }
-    else if ($(event.target).hasClass("button")) {
+    else if ($(event.target).hasClass("note")) {
       noteId = convertNoteIdToInt(event);
       newSeq.toggleNote(noteId, event);
+    }
+    else if ($(event.target).hasClass("instrument")) {
+      $("#i" + currentInstrument).removeClass('selected');
+      currentInstrument = event.target.id.slice(1);
+      $("#i" + currentInstrument).addClass('selected');
+    }
+    else if ($(event.target).hasClass("channel")) {
+      $("#ch" + channel).removeClass('selected');
+      channel = event.target.id.slice(2);
+      $("#ch" + channel).addClass('selected');
     };
   });
 };
@@ -103,11 +132,13 @@ Sequence.prototype.addListener = function () {
 Sequence.prototype.toggleNote = function (noteId, event) {
   if (notes[noteId] === null) {
     notes[noteId] = convertNoteIdToValue(noteId) + 50; // adding 50 converts an ID of 0 to around A3
-    $('#' + event.target.id).html('On');
+    $("#" + noteId).addClass('selected');
+   // $('#' + event.target.id).html('On');
   }
   else {
     notes[noteId] = null;
-    $('#' + event.target.id).html(event.target.id);
+    $("#" + noteId).removeClass('selected');
+    // $('#' + event.target.id).html(event.target.id);
   };
 };
 
@@ -116,16 +147,18 @@ Sequence.prototype.playNotes = function (time) {
   var velocity = 127; // how hard the note hits
   var note = 0;
   MIDI.setVolume(0, 127);
-  for (var i = 0; i < totalOctaveNotes; i++) {
+  MIDI.programChange(channel, currentInstrument); //channel, program
+  for (var i = 0; i < totalOctaveNotes; i++) { // set up new channel for each note played at one time
     if (notes[i + time]) {
       note = notes[i + time]; // the MIDI note
-      MIDI.programChange(i, 0); 
-      $('#' + time).addClass('light');
+      var noteId = time + i
+      $('#' + noteId).removeClass('lightOff');
+      $('#' + noteId).addClass('lightOn');
       setTimeout(function(){
-        $('#' + time).removeClass('light');}, 500);
+        $('#' + noteId).removeClass('lightOn');
+        $('#' + noteId).addClass('lightOff');}, 500);
       // play the note
-      MIDI.noteOn(0, note, velocity, delay);
-      // MIDI.noteOff(0, note, delay + 0.75);     
+      MIDI.noteOn(channel, note, velocity, delay);
     }
   };
 };
